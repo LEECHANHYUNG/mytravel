4bf4f1952a7646d3f231f60ff7cb8a2c98e66e82
LEECHANHYUNG <lsj721538@gmail.com>
2023-06-24 23:31:12 +0900

    feat(googleMaps): google 지도 추가

---
+++ src/client/components/Maps/Maps.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useMemo, useState } from 'react';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import React from 'react';
+import { Spinner } from 'reactstrap';
+
+const Maps = () => {
+  const libraries = useMemo(() => ['places'], []);
+  const [mapCenter, setMapCenter] = useState({ lat: 37.3947, lng: 127.1112 });
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setMapCenter({ lat: latitude, lng: longitude });
+      });
+    }
+  }, []);
+  const mapOptions = useMemo(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    libraries: libraries,
+  });
+  if (!isLoaded) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+  return (
+    <GoogleMap
+      options={mapOptions}
+      zoom={14}
+      center={mapCenter}
+      mapTypeId={google.maps.MapTypeId.ROADMAP}
+      mapContainerStyle={{ width: '428px', height: '926px' }}
+      onLoad={() => console.log('Map Component Loaded...')}
+    />
+  );
+};
+
+export default Maps;
